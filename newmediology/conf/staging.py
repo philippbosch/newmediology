e230e19429755148e48ab047842d7e1159cccbaf
Philipp Bosch <hello@pb.io>
2012-03-01 21:47:38 +0100

    Add static/compress settings for staging env.

--- newmediology/conf/staging.py
+++ newmediology/conf/staging.py
@@ -1,5 +1,5 @@
-from .default import PROJECT_NAME
-
+# from .default import PROJECT_NAME
+# 
 EMAIL_HOST = 'imap.gmail.com'
 EMAIL_HOST_USER = 'test@pb.io'
 EMAIL_HOST_PASSWORD = 'test3579'
@@ -10,9 +10,9 @@ CACHE_BACKEND = 'dummy:///'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-COMPRESS_URL = STATIC_URL = 'http://cdn.newmediology.org.s3.amazonaws.com/'
+STATIC_URL = 'http://cdn.newmediology.org.s3.amazonaws.com/'
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', 'static.tmp')
 # COMPRESS_ROOT = STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', 'static.tmp')
-COMPRESS_STORAGE = '%s.util.storage.CachedS3BotoStorage' % PROJECT_NAME
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
+# COMPRESS_STORAGE = '%s.util.storage.CachedS3BotoStorage' % PROJECT_NAME
+# COMPRESS_ENABLED = True
+# COMPRESS_OFFLINE = True
